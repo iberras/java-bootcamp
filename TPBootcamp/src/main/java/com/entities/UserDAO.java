b40bd5241312758ede4acfc09b8ea123d80d5cc9
Facu <facu.andrade14@gmail.com>
2015-02-09 18:03:53 -0300

    sync2

--- TPBootcamp/src/main/java/com/entities/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.entities;
-
-import java.util.List;
-
-public interface UserDAO {
-	public User getUser(int id);
-	public List<User> getAllUsers();
-	public User getUser(String nickName);
-}
