5a8eda1243a6035715d2ed21b5e002b613c431de
Facu <facu.andrade14@gmail.com>
2015-02-09 18:31:41 -0300

    finish

---
+++ TPBootcamp/src/main/java/com/entities/ShoppingCartEntryPK.java
@@ -0,0 +1,24 @@
+package com.entities;
+
+public class ShoppingCartEntryPK {
+
+	private int idUser;
+	private int idItem;
+
+	public int getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public int getIdItem() {
+		return idItem;
+	}
+
+	public void setIdItem(int idItem) {
+		this.idItem = idItem;
+	}
+
+}
--- TPBootcamp/src/main/java/com/entities/impl/ShoppingCartEntryDAOMySQLImp.java
+++ TPBootcamp/src/main/java/com/entities/impl/ShoppingCartEntryDAOMySQLImp.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
 
 import com.entities.ShoppingCartEntry;
+import com.entities.ShoppingCartEntryPK;
+import com.entities.User;
 
 @Repository
 public class ShoppingCartEntryDAOMySQLImp implements ShoppingCartEntryDAO {
@@ -30,7 +33,10 @@ public class ShoppingCartEntryDAOMySQLImp implements ShoppingCartEntryDAO {
 
 	@Override
 	public void updateItemQuantity(int idUser, int idItem, int quantity) {
-		ShoppingCartEntry entry = entityManager.find(ShoppingCartEntry.class, idUser);
+		ShoppingCartEntryPK pk = new ShoppingCartEntryPK();
+		pk.setIdUser(idUser);
+		pk.setIdItem(idItem);
+		ShoppingCartEntry entry = entityManager.find(ShoppingCartEntry.class, pk);
 		entityManager.getTransaction().begin();
 		entry.setQuantity(quantity);
 		entityManager.getTransaction().commit();
@@ -38,20 +44,30 @@ public class ShoppingCartEntryDAOMySQLImp implements ShoppingCartEntryDAO {
 
 	@Override
 	public void clearCart(int idUser) {
-		// TODO Auto-generated method stub
-
+		Query query = (Query) entityManager.createQuery(
+			      "delete from ShoppingCartEntry where idUser = "+ idUser + ") ");
+		query.executeUpdate();
 	}
 
 	@Override
 	public void deleteItem(int idUser, int idItem) {
-		// TODO Auto-generated method stub
-
+		ShoppingCartEntryPK pk = new ShoppingCartEntryPK();
+		pk.setIdUser(idUser);
+		pk.setIdItem(idItem);
+		ShoppingCartEntry entry = entityManager.find(ShoppingCartEntry.class, pk);
+		entityManager.getTransaction().begin();
+		entityManager.remove(entry);
+		entityManager.getTransaction().commit();
 	}
 
 	@Override
 	public List<ShoppingCartEntry> getShoppingCart(int idUser) {
-		// TODO Auto-generated method stub
-		return null;
+		Query q1 = (Query) entityManager.createQuery("SELECT e "
+				+ "from ShoppingCartEntry e "
+				+ "where  (e.idUser = "+ idUser + ") ");
+				
+		List<ShoppingCartEntry> queryResult = q1.getResultList();
+		return queryResult;
 	}
 
 }
